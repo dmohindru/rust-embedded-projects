6edf6c3f1e5dd2b8743a8121143a38a0b99c1316
Dhruv Mohindru <dmohindru@gmail.com>
2025-11-27 22:45:51 +1100

    Commit

---
+++ microbit-v2/projects/project01_led_marix_projects/led_matrix_project1/README.md
@@ -0,0 +1,16 @@
+### Introduction
+
+This project will demo the coordination between button press updating display LED Matrix.
+
+### Resources used
+
+- Push button
+- Led Matrix
+- Mutex
+
+https://blog.theembeddedrustacean.com/sharing-data-among-tasks-in-rust-embassy-synchronization-primitives
+
+how to create two dimensional array uint8
+vectors in embedded world
+
+### Solution approach
--- microbit-v2/projects/project01_led_marix_projects/led_matrix_project1/src/main.rs
+++ microbit-v2/projects/project01_led_marix_projects/led_matrix_project1/src/main.rs
@@ -3,22 +3,54 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
+use embassy_nrf::gpio::{AnyPin, Input, Output, Pull};
+use embassy_sync::{
+    blocking_mutex::raw::ThreadModeRawMutex,
+    channel::{Channel, Receiver, Sender},
+};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
-/// Fast task: logs every 500ms
+enum Direction {
+    Left,
+    Right,
+}
+
+static CHANNEL: Channel<ThreadModeRawMutex, Direction, 2> = Channel::new();
+
+// Button task that would detect button press and send a message on a channel
+// Pool 2 required for button A, button B
+// Params
+// 1. Button pin
+// 2. Channel sender
+// 3. Value to send
 #[embassy_executor::task]
-async fn task_fast() {
+async fn button_task(
+    sender: Sender<'static, ThreadModeRawMutex, Direction, 2>,
+    button: Input<'static>,
+    value: Direction,
+) {
     loop {
-        info!("Fast task: Hello from Embassy! (500ms interval)");
-        Timer::after(Duration::from_millis(500)).await;
+        button.wait_for_high().await;
+
+        Timer::after_millis(20).await;
+
+        if button.is_high() {
+            sender.send(value).await;
+        }
+
+        button.wait_for_low().await;
     }
 }
 
-/// Slow task: logs every 2 seconds
+// Receiver task to receive button press and do its business logic
+// Params
+// 1. Channel Receiver
 #[embassy_executor::task]
-async fn task_slow() {
+async fn button_receiver(receiver: Receiver<'static, ThreadModeRawMutex, Direction, 2>) {
     loop {
+        let button_pressed = receiver.receive().await;
+
         info!("Slow task: Hello from microbit v2! (2s interval)");
         Timer::after(Duration::from_secs(2)).await;
     }
@@ -26,18 +58,22 @@ async fn task_slow() {
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
-    embassy_nrf::init(Default::default());
-    info!("=== Embassy Async Runtime Demo ===");
-    info!("Starting two concurrent tasks...");
+    let p = embassy_nrf::init(Default::default());
+    let btn_a = Input::new(p.P0_14, Pull::Up);
+    let btn_b = Input::new(p.P0_23, Pull::Up);
+    let sender = CHANNEL.sender();
+    let receiver = CHANNEL.recv();
 
-    // Spawn both tasks on the executor
-    spawner
-        .spawn(task_fast())
-        .expect("Failed to spawn fast task");
-    spawner
-        .spawn(task_slow())
-        .expect("Failed to spawn slow task");
-
-    // Main task completes, executor keeps running the spawned tasks
-    info!("Main task complete, executor running...");
+    spawner.spawn(button_task(sender, btn_a, Direction::Left));
+    spawner.spawn(button_task(sender, btn_b, Direction::Left));
 }
+
+/*
+TODO
+1. Create a data structure
+    1.1 To hold all frame buffer data to be used by application. In this case list of 5 frame data (two dimensional array of unsigned int or even bool)
+    1.2 function to get the current active frame
+    1.3 function to move the pointer to other frames as per business logic avoiding data race condition
+2. Create a frame rendered that takes the current frame data and renders it
+
+*/
