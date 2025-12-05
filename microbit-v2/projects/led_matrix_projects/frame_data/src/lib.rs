21f99b01cf87a4eff5edde0da1e06e9742d20684
Dhruv Mohindru <dmohindru@gmail.com>
2025-12-05 19:02:45 +1100

    Got into working state firmware

--- microbit-v2/projects/led_matrix_projects/frame_data/src/lib.rs
+++ microbit-v2/projects/led_matrix_projects/frame_data/src/lib.rs
@@ -1,10 +1,10 @@
 #![no_std]
-extern crate alloc;
+// extern crate alloc;
 
 // #[cfg(feature = "std")]
 #[cfg(test)]
 extern crate std;
 
-pub mod display_driver;
+// pub mod display_driver;
 pub mod frame;
-pub mod refresh_engine;
+// pub mod refresh_engine;
--- microbit-v2/projects/led_matrix_projects/led_matrix_project1/src/main.rs
+++ microbit-v2/projects/led_matrix_projects/led_matrix_project1/src/main.rs
@@ -3,12 +3,13 @@
 
 use defmt::info;
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{AnyPin, Input, Output, Pull};
+use embassy_nrf::gpio::{Input, Output, Pull};
 use embassy_sync::{
     blocking_mutex::raw::ThreadModeRawMutex,
     channel::{Channel, Receiver, Sender},
+    mutex::Mutex,
 };
-use embassy_time::{Duration, Timer};
+use embassy_time::Timer;
 use frame_data::frame::{Direction, Frame, FrameData};
 use {defmt_rtt as _, panic_probe as _};
 
@@ -21,6 +22,43 @@ const COL4: Frame = [0x02, 0x02, 0x02, 0x02, 0x02];
 const COL5: Frame = [0x01, 0x01, 0x01, 0x01, 0x01];
 
 static FRAMES: &[Frame] = &[COL1, COL2, COL3, COL4, COL5];
+
+type FrameDataType = Mutex<ThreadModeRawMutex, Option<FrameData<5>>>;
+
+static FRAME_DATA: FrameDataType = Mutex::new(None);
+
+//--------------------
+// LED Refresh Task
+//--------------------
+#[embassy_executor::task]
+async fn led_refresh_task(mut rows: [Output<'static>; 5], mut cols: [Output<'static>; 5]) {
+    loop {
+        // Read frame once per scan → lock only once
+        let frame = {
+            let frame_opt = FRAME_DATA.lock().await;
+            frame_opt.as_ref().unwrap().current_frame().clone()
+        };
+
+        for row in 0..5 {
+            for r in &mut rows {
+                r.set_high();
+            }
+
+            rows[row].set_low();
+            let row_bits = frame[row];
+
+            for col in 0..5 {
+                if (row_bits & (1 << (4 - col))) != 0 {
+                    cols[col].set_low();
+                } else {
+                    cols[col].set_high();
+                }
+            }
+
+            Timer::after_micros(300).await;
+        }
+    }
+}
 
 //--------------------
 // Button Task
@@ -46,14 +84,16 @@ async fn button_task(
 // Receiver Task
 //--------------------
 #[embassy_executor::task]
-async fn button_receiver(
-    receiver: Receiver<'static, ThreadModeRawMutex, Direction, 2>,
-    mut frame_data: FrameData<5>,
-) {
+async fn button_receiver(receiver: Receiver<'static, ThreadModeRawMutex, Direction, 2>) {
     loop {
         let button_pressed = receiver.receive().await;
         info!("Button pressed {}", button_pressed);
-        frame_data.move_index(button_pressed);
+        {
+            let mut frame_data_option = FRAME_DATA.lock().await;
+            if let Some(frame_data) = frame_data_option.as_mut() {
+                frame_data.move_index(button_pressed);
+            }
+        }
     }
 }
 
@@ -67,6 +107,68 @@ async fn main(spawner: Spawner) {
     let receiver = CHANNEL.receiver();
 
     let frame_data = FrameData::<5>::new(&FRAMES);
+    {
+        *(FRAME_DATA.lock().await) = Some(frame_data);
+    }
+
+    // -----------------
+    // LED matrix pins
+    // -----------------
+    let rows = [
+        Output::new(
+            p.P0_21,
+            embassy_nrf::gpio::Level::High,
+            embassy_nrf::gpio::OutputDrive::Standard,
+        ),
+        Output::new(
+            p.P0_22,
+            embassy_nrf::gpio::Level::High,
+            embassy_nrf::gpio::OutputDrive::Standard,
+        ),
+        Output::new(
+            p.P0_15,
+            embassy_nrf::gpio::Level::High,
+            embassy_nrf::gpio::OutputDrive::Standard,
+        ),
+        Output::new(
+            p.P0_24,
+            embassy_nrf::gpio::Level::High,
+            embassy_nrf::gpio::OutputDrive::Standard,
+        ),
+        Output::new(
+            p.P0_19,
+            embassy_nrf::gpio::Level::High,
+            embassy_nrf::gpio::OutputDrive::Standard,
+        ),
+    ];
+
+    let cols = [
+        Output::new(
+            p.P0_28,
+            embassy_nrf::gpio::Level::High,
+            embassy_nrf::gpio::OutputDrive::Standard,
+        ),
+        Output::new(
+            p.P0_11,
+            embassy_nrf::gpio::Level::High,
+            embassy_nrf::gpio::OutputDrive::Standard,
+        ),
+        Output::new(
+            p.P0_31,
+            embassy_nrf::gpio::Level::High,
+            embassy_nrf::gpio::OutputDrive::Standard,
+        ),
+        Output::new(
+            p.P1_05,
+            embassy_nrf::gpio::Level::High,
+            embassy_nrf::gpio::OutputDrive::Standard,
+        ),
+        Output::new(
+            p.P0_30,
+            embassy_nrf::gpio::Level::High,
+            embassy_nrf::gpio::OutputDrive::Standard,
+        ),
+    ];
 
     spawner
         .spawn(button_task(sender_a, btn_a, Direction::Left))
@@ -75,6 +177,10 @@ async fn main(spawner: Spawner) {
         .spawn(button_task(sender_b, btn_b, Direction::Right))
         .expect("Failed to spawn button B task");
     spawner
-        .spawn(button_receiver(receiver, frame_data))
+        .spawn(button_receiver(receiver))
         .expect("Failed to spawn button receiver task");
+
+    spawner
+        .spawn(led_refresh_task(rows, cols))
+        .expect("Failed to spawn led refresh task");
 }
