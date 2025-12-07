76c0151d163c9f7889e19ec39515ac4fd154ea69
Dhruv Mohindru <dmohindru@gmail.com>
2025-12-08 10:42:58 +1100

    test code refactor

--- microbit-v2/projects/led_matrix_projects/common_utils/src/frame/frame_cursor.rs
+++ microbit-v2/projects/led_matrix_projects/common_utils/src/frame/frame_cursor.rs
@@ -49,35 +49,30 @@ impl<const N: usize, const R: usize, const C: usize> FrameCursor<N, R, C> {
 
 #[cfg(test)]
 mod tests {
-    // Forces std mode for the module
-    // extern crate std;
     use super::*;
     static F1_DATA: [u32; 5] = [1, 1, 1, 1, 1];
     static F2_DATA: [u32; 5] = [2, 2, 2, 2, 2];
     static F3_DATA: [u32; 5] = [3, 3, 3, 3, 3];
+    fn frame(data: [u32; 5]) -> Frame<5, 5> {
+        Frame::<5, 5>::new(data)
+    }
 
     fn before_each() -> FrameCursor<3, 5, 5> {
-        let f1: Frame<5, 5> = Frame::<5, 5>::new(F1_DATA);
-        let f2: Frame<5, 5> = Frame::<5, 5>::new(F2_DATA);
-        let f3: Frame<5, 5> = Frame::<5, 5>::new(F3_DATA);
-
-        let frames = [f1, f2, f3];
+        let frames = [frame(F1_DATA), frame(F2_DATA), frame(F3_DATA)];
         FrameCursor::<3, 5, 5>::new(&frames)
     }
 
     #[test]
     fn should_return_center_frame() {
         let frame_cursor = before_each();
-        let f2: Frame<5, 5> = Frame::<5, 5>::new(F2_DATA);
-        assert_eq!(&f2, frame_cursor.current_frame());
+        assert_eq!(&frame(F2_DATA), frame_cursor.current_frame());
     }
 
     #[test]
     fn should_move_frame_to_left() {
         let mut frame_cursor = before_each();
         frame_cursor.move_index(Direction::Left);
-        let f1: Frame<5, 5> = Frame::<5, 5>::new(F1_DATA);
-        assert_eq!(&f1, frame_cursor.current_frame());
+        assert_eq!(&frame(F1_DATA), frame_cursor.current_frame());
     }
 
     #[test]
@@ -85,16 +80,14 @@ mod tests {
         let mut frame_cursor = before_each();
         frame_cursor.move_index(Direction::Left);
         frame_cursor.move_index(Direction::Left);
-        let f1: Frame<5, 5> = Frame::<5, 5>::new(F1_DATA);
-        assert_eq!(&f1, frame_cursor.current_frame());
+        assert_eq!(&frame(F1_DATA), frame_cursor.current_frame());
     }
 
     #[test]
     fn should_move_frame_to_right() {
         let mut frame_cursor = before_each();
         frame_cursor.move_index(Direction::Right);
-        let f3: Frame<5, 5> = Frame::<5, 5>::new(F3_DATA);
-        assert_eq!(&f3, frame_cursor.current_frame());
+        assert_eq!(&frame(F3_DATA), frame_cursor.current_frame());
     }
 
     #[test]
@@ -102,7 +95,6 @@ mod tests {
         let mut frame_cursor = before_each();
         frame_cursor.move_index(Direction::Right);
         frame_cursor.move_index(Direction::Right);
-        let f3: Frame<5, 5> = Frame::<5, 5>::new(F3_DATA);
-        assert_eq!(&f3, frame_cursor.current_frame());
+        assert_eq!(&frame(F3_DATA), frame_cursor.current_frame());
     }
 }
