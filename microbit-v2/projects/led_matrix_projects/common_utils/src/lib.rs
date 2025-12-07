c04092c10df73a9df68b022eb4021dda111749fe
Dhruv Mohindru <dmohindru@gmail.com>
2025-12-07 17:30:16 +1100

    Some enhancements

--- microbit-v2/projects/led_matrix_projects/common_utils/src/lib.rs
+++ microbit-v2/projects/led_matrix_projects/common_utils/src/lib.rs
@@ -1,11 +1,5 @@
 #![no_std]
-// extern crate alloc;
-
-// #[cfg(feature = "std")]
-#[cfg(test)]
-extern crate std;
 
 pub mod button;
 pub mod display_driver;
 pub mod frame;
-// pub mod refresh_engine;
