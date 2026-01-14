72022af5a23ee22bff7fa6fbcc7f8a995feea434
Dhruv Mohindru <dmohindru@gmail.com>
2026-01-14 12:44:12 +1100

    Created step circular buffer

--- microbit-v2/projects/led_matrix_projects/common_utils/src/utils/mod.rs
+++ microbit-v2/projects/led_matrix_projects/common_utils/src/utils/mod.rs
@@ -1,1 +1,2 @@
 mod step_cursor_circular;
+pub use step_cursor_circular::StepCursorCircular;
--- microbit-v2/projects/led_matrix_projects/common_utils/src/utils/step_cursor_circular.rs
+++ microbit-v2/projects/led_matrix_projects/common_utils/src/utils/step_cursor_circular.rs
@@ -1,42 +1,38 @@
 use crate::frame::Direction;
 
 pub struct StepCursorCircular {
-    initial_value: i16,
+    max_value: i16,
     step_size: i16,
-    current_value: i16,
+    num_steps: i16,
+    current_step: i16,
 }
 
 impl StepCursorCircular {
-    pub fn new(initial_value: i16, step_size: i16) -> Self {
+    pub fn new(max_value: i16, min_value: i16, step_size: i16, num_steps: i16) -> Self {
         assert!(
-            initial_value > 0,
-            "initial_value should be greater than zero"
+            max_value - step_size * (num_steps - 1) == min_value,
+            "Invalid values for step size and num_steps to reach min_value"
         );
-        assert!(step_size > 0, "step_size should be greater than zero");
+
         Self {
-            initial_value,
+            max_value,
             step_size,
-            current_value: initial_value,
+            num_steps,
+            current_step: 0,
         }
     }
 
     pub fn current_value(&self) -> i16 {
-        self.current_value
+        self.max_value - self.current_step * self.step_size
     }
 
     pub fn move_step(&mut self, direction: Direction) {
         match direction {
             Direction::Left => {
-                self.current_value -= self.step_size;
-                if self.current_value < 0 {
-                    self.current_value = self.initial_value;
-                }
+                self.current_step = (self.current_step + 1) % self.num_steps;
             }
             Direction::Right => {
-                self.current_value += self.step_size;
-                if self.current_value > self.initial_value {
-                    self.current_value = 0;
-                }
+                self.current_step = (self.current_step + self.num_steps - 1) % self.num_steps;
             }
         }
     }
@@ -45,32 +41,31 @@ impl StepCursorCircular {
 #[cfg(test)]
 mod tests {
     use super::*;
-    fn get_circular_step_cursor(initial_value: i16, step_size: i16) -> StepCursorCircular {
-        StepCursorCircular::new(initial_value, step_size)
+    fn get_circular_step_cursor(
+        max_value: i16,
+        min_value: i16,
+        step_size: i16,
+        num_steps: i16,
+    ) -> StepCursorCircular {
+        StepCursorCircular::new(max_value, min_value, step_size, num_steps)
     }
 
     #[test]
-    #[should_panic(expected = "initial_value should be greater than zero")]
+    #[should_panic(expected = "Invalid values for step size and num_steps to reach min_value")]
     fn should_panic_when_initial_value_less_than_or_equal_zero() {
-        get_circular_step_cursor(-1, 100);
-    }
-
-    #[test]
-    #[should_panic(expected = "step_size should be greater than zero")]
-    fn should_panic_when_step_size_value_less_than_or_equal_zero() {
-        get_circular_step_cursor(1000, 0);
+        get_circular_step_cursor(1000, 500, 250, 4);
     }
 
     #[test]
     fn should_return_current_value_for_circular_cursor() {
-        let step_cursor = get_circular_step_cursor(1000, 200);
+        let step_cursor = get_circular_step_cursor(1000, 400, 200, 4);
         let current_value = step_cursor.current_value();
         assert_eq!(1000, current_value);
     }
 
     #[test]
     fn should_step_down_current_value_for_circular_cursor() {
-        let mut step_cursor = get_circular_step_cursor(1000, 200);
+        let mut step_cursor = get_circular_step_cursor(1000, 400, 200, 4);
         for _ in 0..2 {
             step_cursor.move_step(Direction::Left);
         }
@@ -79,9 +74,19 @@ mod tests {
     }
 
     #[test]
+    fn should_step_down_min_value_for_circular_cursor() {
+        let mut step_cursor = get_circular_step_cursor(1000, 400, 200, 4);
+        for _ in 0..3 {
+            step_cursor.move_step(Direction::Left);
+        }
+        let current_value = step_cursor.current_value();
+        assert_eq!(400, current_value);
+    }
+
+    #[test]
     fn should_circular_rotate_current_value_when_step_down_past_zero() {
-        let mut step_cursor = get_circular_step_cursor(1000, 200);
-        for _ in 0..7 {
+        let mut step_cursor = get_circular_step_cursor(1000, 400, 200, 4);
+        for _ in 0..5 {
             step_cursor.move_step(Direction::Left);
         }
         let current_value = step_cursor.current_value();
@@ -90,7 +95,7 @@ mod tests {
 
     #[test]
     fn should_step_up_current_value_for_circular_cursor() {
-        let mut step_cursor = get_circular_step_cursor(1000, 200);
+        let mut step_cursor = get_circular_step_cursor(1000, 400, 200, 4);
         for _ in 0..2 {
             step_cursor.move_step(Direction::Left);
         }
@@ -103,11 +108,11 @@ mod tests {
 
     #[test]
     fn should_circular_rotate_current_value_when_step_up_past_initial_value() {
-        let mut step_cursor = get_circular_step_cursor(1000, 200);
+        let mut step_cursor = get_circular_step_cursor(1000, 400, 200, 4);
         for _ in 0..2 {
             step_cursor.move_step(Direction::Right);
         }
         let current_value = step_cursor.current_value();
-        assert_eq!(200, current_value);
+        assert_eq!(600, current_value);
     }
 }
