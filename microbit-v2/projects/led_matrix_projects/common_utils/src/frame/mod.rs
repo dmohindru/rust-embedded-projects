e32fc211492b64b10e37eb99ffd6031f93c838f4
Dhruv Mohindru <dmohindru@gmail.com>
2026-01-19 18:17:04 +1100

    Completed project 4

--- microbit-v2/projects/led_matrix_projects/common_utils/src/frame/mod.rs
+++ microbit-v2/projects/led_matrix_projects/common_utils/src/frame/mod.rs
@@ -1,10 +1,13 @@
 mod frame;
 mod frame_cursor;
 mod frame_cursor_circular;
+mod frame_decode;
+
 use defmt::Format;
 pub use frame::Frame;
 pub use frame_cursor::FrameCursor;
 pub use frame_cursor_circular::FrameCursorCircular;
+pub use frame_decode::decode_frames;
 
 #[derive(Format, Clone, Copy)]
 pub enum Direction {
