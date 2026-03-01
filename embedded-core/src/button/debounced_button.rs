cff52e93dacddcd17f033b1da65c307f88d181d1
Dhruv Mohindru <dmohindru@gmail.com>
2026-03-02 10:50:39 +1100

    Created tests for debounced button

--- embedded-core/src/button/debounced_button.rs
+++ embedded-core/src/button/debounced_button.rs
@@ -26,46 +26,68 @@ where
         Fut: core::future::Future<Output = ()>,
     {
         loop {
-            self.button.wait_for_low().await.unwrap();
-
-            self.delay.delay_ms(self.debounce_ms).await;
-
-            if self.button.is_low().unwrap() {
-                on_press().await;
-            }
-
-            self.button.wait_for_high().await.unwrap();
+            self.wait_once(&mut on_press).await;
         }
+    }
+
+    async fn wait_once<F, Fut>(&mut self, on_press: &mut F)
+    where
+        F: FnMut() -> Fut,
+        Fut: core::future::Future<Output = ()>,
+    {
+        self.button.wait_for_low().await.unwrap();
+        self.delay.delay_ms(self.debounce_ms).await;
+
+        if self.button.is_low().unwrap() {
+            on_press().await;
+        }
+
+        self.button.wait_for_high().await.unwrap();
+    }
+}
+
+#[cfg(test)]
+impl<P, D> DebouncedButton<P, D> {
+    pub fn free(self) -> (P, D) {
+        (self.button, self.delay)
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use core::cell::Cell;
+    use embedded_hal_mock::eh1::digital::Mock as PinMock;
+    use embedded_hal_mock::eh1::digital::{State, Transaction};
 
     struct FakeDelay;
+
     impl DelayNs for FakeDelay {
-        async fn delay_ns(&mut self, ns: u32) {}
+        async fn delay_ns(&mut self, _ns: u32) {}
     }
 
     #[tokio::test]
     async fn debounced_button_test() {
-        use embedded_hal_mock::eh1::digital::Mock as PinMock;
-        use embedded_hal_mock::eh1::digital::{Edge, State, Transaction};
         let expectations = [
-            Transaction::wait_for_edge(Edge::Falling),
-            Transaction::get(State::High),
-            Transaction::wait_for_edge(Edge::Rising),
+            Transaction::wait_for_state(State::Low),
+            Transaction::get(State::Low),
+            Transaction::wait_for_state(State::High),
         ];
+
         let mock_pin = PinMock::new(&expectations);
         let mut button = DebouncedButton::new(mock_pin, FakeDelay, 10);
-        let mut called = false;
-        button
-            .wait(|| async {
-                // called = true;
-            })
-            .await;
 
-        assert!(called);
+        let called = Cell::new(false);
+
+        let mut callback = || async {
+            called.set(true);
+        };
+
+        button.wait_once(&mut callback).await;
+
+        let (mut mock_pin, _) = button.free();
+        mock_pin.done();
+
+        assert!(called.get());
     }
 }
