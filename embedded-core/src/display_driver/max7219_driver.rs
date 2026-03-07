252d4172b560890064809f91c9a84fe4a4fa814b
Dhruv Mohindru <dmohindru@gmail.com>
2026-03-08 00:22:20 +1100

    Wrote tests for Max7219 driver

--- embedded-core/src/display_driver/max7219_driver.rs
+++ embedded-core/src/display_driver/max7219_driver.rs
@@ -79,16 +79,14 @@ mod tests {
 
     #[tokio::test]
     async fn should_initialize_with_proper_commands() {
-        let expectations: [SpiTransaction<u8>; 8] = [
-            SpiTransaction::transaction_start(),
-            SpiTransaction::write_vec(vec![0x0F, 0x00]),
-            SpiTransaction::write_vec(vec![0x0C, 0x00]),
-            SpiTransaction::write_vec(vec![0x09, 0x00]),
-            SpiTransaction::write_vec(vec![0x0B, 0x07]),
-            SpiTransaction::write_vec(vec![0x0A, 0x08]),
-            SpiTransaction::write_vec(vec![0x0C, 0x01]),
-            SpiTransaction::transaction_end(),
-        ];
+        let expectations = build_expected_transactions(vec![
+            vec![0x0F, 0x00],
+            vec![0x0C, 0x00],
+            vec![0x09, 0x00],
+            vec![0x0B, 0x07],
+            vec![0x0A, 0x08],
+            vec![0x0C, 0x01],
+        ]);
 
         let spi = SpiMock::new(&expectations);
 
@@ -96,5 +94,58 @@ mod tests {
         max7219_device.initialize().await.unwrap();
 
         max7219_device.free().done();
+    }
+
+    #[tokio::test]
+    async fn should_write_frame_data_with_proper_commands() {
+        let frame_data = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07];
+        let frame: Frame<8, 8> = Frame::new(frame_data.clone());
+        let mut transaction_data = Vec::<Vec<u8>>::new();
+        for data in frame_data {
+            let addr = (data + 1) as u8;
+            transaction_data.push(vec![addr, data as u8]);
+        }
+        let expectations = build_expected_transactions(transaction_data);
+        let spi = SpiMock::new(&expectations);
+        let mut max7219_device = Max7219::<_, 8, 8>::new(spi);
+        max7219_device.write_bitmap(&frame).await.unwrap();
+        max7219_device.free().done();
+    }
+
+    #[tokio::test]
+    async fn should_clear_frame_data_with_proper_commands() {
+        let mut transaction_data = Vec::<Vec<u8>>::new();
+        for n in 1..9 {
+            transaction_data.push(vec![n as u8, 0x00]);
+        }
+        let expectations = build_expected_transactions(transaction_data);
+        let spi = SpiMock::new(&expectations);
+        let mut max7219_device = Max7219::<_, 8, 8>::new(spi);
+        max7219_device.clear().await.unwrap();
+        max7219_device.free().done();
+    }
+
+    #[tokio::test]
+    async fn should_set_led_intensity_correctly() {
+        let transaction_data: Vec<Vec<u8>> = vec![vec![0x0A, 0x08]];
+        let expectations = build_expected_transactions(transaction_data);
+        let spi = SpiMock::new(&expectations);
+        let mut max7219_device = Max7219::<_, 8, 8>::new(spi);
+        max7219_device.set_intensity(0xF8).await.unwrap();
+        max7219_device.free().done();
+    }
+
+    fn build_expected_transactions(transaction_data: Vec<Vec<u8>>) -> Vec<SpiTransaction<u8>> {
+        transaction_data
+            .iter()
+            .map(|t| {
+                vec![
+                    SpiTransaction::transaction_start(),
+                    SpiTransaction::write_vec(t.to_vec()),
+                    SpiTransaction::transaction_end(),
+                ]
+            })
+            .flat_map(|f| f)
+            .collect()
     }
 }
