ca610f71c3cf19207e8406e917d4ceee1009b7b7
Dhruv Mohindru <dmohindru@gmail.com>
2026-03-17 20:32:09 +1100

    started project 2

---
+++ rpi-pico/projects/project01_buttons/src/main.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_rp::gpio::{Level, Output};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    let mut led = Output::new(p.PIN_25, Level::Low);
+
+    loop {
+        info!("from project 1led on!");
+        led.set_high();
+        Timer::after_secs(1).await;
+
+        info!("from project 1led off!");
+        led.set_low();
+        Timer::after_secs(1).await;
+    }
+}
