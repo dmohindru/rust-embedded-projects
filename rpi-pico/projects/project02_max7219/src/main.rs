7a6719948739d1e76c94782223a266e10802b2f6
Dhruv Mohindru <dmohindru@gmail.com>
2026-03-20 18:59:33 +1100

    Got a compiling code

--- rpi-pico/projects/project02_max7219/src/main.rs
+++ rpi-pico/projects/project02_max7219/src/main.rs
@@ -2,53 +2,69 @@
 #![no_main]
 
 use defmt::info;
+use embassy_embedded_hal::shared_bus::asynch::spi::SpiDevice;
 use embassy_executor::Spawner;
-use embassy_rp::gpio::{Input, Pull};
-use embassy_rp::spi::{Config, Spi};
-use embassy_time::{Delay, Timer};
-use embedded_core::button::DebouncedButton;
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::peripherals::SPI0;
+use embassy_rp::spi::{Async, Config, Spi};
+use embassy_sync::blocking_mutex::raw::NoopRawMutex;
+use embassy_sync::mutex::Mutex;
+use embassy_time::Timer;
+use embedded_core::display_driver::Max7219;
+use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
 
+// SPI type
+type MySpi = Spi<'static, SPI0, Async>;
+
+// SPI + CS shared container
+// TODO: Bus in nothing more than a Mutex stored in Global storage
+static SPI_BUS: StaticCell<Mutex<NoopRawMutex, MySpi>> = StaticCell::new();
+
+// Max7219 Type
+type Max7219Device = SpiDevice<'static, NoopRawMutex, MySpi, Output<'static>>;
+
 #[embassy_executor::task]
-async fn button_task(
-    mut button: DebouncedButton<Input<'static>, Delay>,
-    mut spi: Spi<'static, embassy_rp::peripherals::SPI1, embassy_rp::spi::Async>,
-) {
+async fn spi_init_task(mut spi_device: Max7219<Max7219Device, 8, 8>) {
     loop {
-        // button
-        //     .wait(|| async {
-        //         info!("Button pressed");
-        //     })
-        //     .await;
-        let tx_buf = [1_u8, 2, 3, 4, 5, 6];
-        let mut rx_buf = [0_u8; 6];
-        spi.transfer(&mut rx_buf, &tx_buf).await.unwrap();
-        info!("{:?}", rx_buf);
-        Timer::after_secs(2).await;
+        spi_device.initialize().await.unwrap();
+        info!("Initialization commands written");
+        Timer::after_secs(10).await;
     }
 }
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let miso = p.PIN_12;
-    let mosi = p.PIN_11;
-    let clk = p.PIN_10;
 
-    let btn_a = Input::new(p.PIN_15, Pull::Up);
-    let debounced_btn_a = DebouncedButton::new(btn_a, Delay, 20);
+    // SPI Config
+    let mut spi_config = Config::default();
+    spi_config.frequency = 8_000_000;
+    spi_config.phase = embassy_rp::spi::Phase::CaptureOnFirstTransition;
+    spi_config.polarity = embassy_rp::spi::Polarity::IdleLow;
 
-    let spi = Spi::new(
-        p.SPI1,
-        clk,
-        mosi,
-        miso,
-        p.DMA_CH0,
-        p.DMA_CH1,
-        Config::default(),
-    );
+    // Chip select pin
+    let cs = Output::new(p.PIN_20, Level::High);
+
+    // SPI Pins
+    let clk = p.PIN_18;
+    let tx = p.PIN_19;
+
+    // SPI device
+    let spi = Spi::new(p.SPI0, clk, tx, p.PIN_16, p.DMA_CH0, p.DMA_CH1, spi_config);
+
+    // Shared bus
+    // TODO: how did spi_but mutex got type of NoopRawMutex
+    let spi_bus = Mutex::new(spi);
+    let spi_bus = SPI_BUS.init(spi_bus);
+
+    // // Device with CS handling
+    let spi_device = SpiDevice::new(spi_bus, cs);
+
+    // Max7219 Device
+    let driver: Max7219<Max7219Device, 8, 8> = Max7219::new(spi_device);
 
     spawner
-        .spawn(button_task(debounced_btn_a, spi))
-        .expect("Failed to spawn button a task");
+        .spawn(spi_init_task(driver))
+        .expect("Failed to spawn spi_init_task");
 }
