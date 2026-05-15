8c2fcf1a8ce5cb473447bb774d97734a459eb2d6
Dhruv Mohindru <dmohindru@gmail.com>
2026-05-15 17:52:52 +1000

    Making some progress in writing test code

--- embedded-core/src/shift_register/hc595.rs
+++ embedded-core/src/shift_register/hc595.rs
@@ -12,6 +12,7 @@ SRCLR   -> Driver handled
 OE      -> Driver handled
 */
 
+#[derive(Debug)]
 pub enum Error<SpiError, PinError> {
     Spi(SpiError),
     Pin(PinError),
@@ -91,27 +92,63 @@ where
 }
 
 #[cfg(test)]
+impl<SPI, PIN, const N: usize> Hc595<SPI, PIN, N>
+where
+    SPI: SpiDevice,
+    PIN: OutputPin,
+{
+    pub fn free(self) -> Self {
+        Hc595 {
+            device: self.device,
+            latch: self.latch,
+            output_enable: self.output_enable,
+            register_clear: self.register_clear,
+        }
+    }
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
+    use embedded_hal_mock::common::Generic;
     use embedded_hal_mock::eh1::digital::{
         Mock as PinMock, State as PinState, Transaction as PinTransaction,
     };
     use embedded_hal_mock::eh1::spi::{Mock as SpiMock, Transaction as SpiTransaction};
+    use embedded_hal_mock::eh1::MockError;
+    use std::io::ErrorKind;
 
     #[test]
     fn should_create_new_instance_initialize_properly() {
-        let mut latch_pin = PinMock::new(&[PinTransaction::set(PinState::Low)]);
-        let mut output_enable_pin = PinMock::new(&[PinTransaction::set(PinState::Low)]);
-        let mut register_clear_pin = PinMock::new(&[PinTransaction::set(PinState::High)]);
+        let latch_pin = PinMock::new(&[PinTransaction::set(PinState::Low)]);
+        let output_enable_pin = PinMock::new(&[PinTransaction::set(PinState::Low)]);
+        let register_clear_pin = PinMock::new(&[PinTransaction::set(PinState::High)]);
+        let spi: Generic<SpiTransaction<u8>> = SpiMock::new(&[]);
 
-        latch_pin.done();
-        output_enable_pin.done();
-        register_clear_pin.done();
+        let hc595 = Hc595::<_, _, 1>::new(
+            spi,
+            latch_pin,
+            Some(output_enable_pin),
+            Some(register_clear_pin),
+        )
+        .unwrap();
+
+        let mut hc595 = hc595.free();
+
+        hc595.device.done();
+        hc595.latch.done();
+        hc595.output_enable.unwrap().done();
+        hc595.register_clear.unwrap().done();
     }
 
     #[test]
     fn should_return_error_during_initialize_for_latch_pin_error() {
-        todo!()
+        let latch_pin = PinMock::new(&[
+            PinTransaction::set(PinState::Low).with_error(MockError::Io(ErrorKind::Other))
+        ]);
+        let spi: Generic<SpiTransaction<u8>> = SpiMock::new(&[]);
+        let hc595 = Hc595::<_, _, 1>::new(spi, latch_pin, None, None);
+        assert!(hc595.is_err());
     }
 
     #[test]
