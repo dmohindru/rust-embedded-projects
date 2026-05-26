670a90ecca9b2076537fcd92018129f6e7a9b050
Dhruv Mohindru <dmohindru@gmail.com>
2026-05-26 19:54:03 +1000

    making progress

---
+++ embedded-core/src/display_driver/ht16k33_driver.rs
@@ -0,0 +1,12 @@
+/*
+Start up routine
+1. System setup: 0x21 → oscillator ON
+2. Dimming set: 0xEF → full brightness
+3. Display setup/blink set: 0x81 → display ON
+*/
+
+/*
+Data writing routine
+1. Display data Address pointer: 0x00
+2. Write eight bytes of data
+*/
