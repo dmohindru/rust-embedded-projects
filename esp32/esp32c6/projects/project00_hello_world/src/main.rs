1bc4e37e68208ebdff5a598b3b1ff5e0c6f663b9
Dhruv Mohindru <dmohindru@gmail.com>
2026-06-04 18:38:31 +1000

    Some more updates

--- esp32/esp32c6/projects/project00_hello_world/src/main.rs
+++ esp32/esp32c6/projects/project00_hello_world/src/main.rs
@@ -2,8 +2,10 @@
 #![no_main]
 
 use embassy_executor::Spawner;
-use esp_hal::clock::CpuClock;
+use embassy_time::Timer;
 use esp_hal::timer::timg::TimerGroup;
+use esp_hal::{Config, clock::CpuClock, init, interrupt::software::SoftwareInterruptControl};
+use esp_rtos::start;
 use rtt_target::rprintln;
 
 extern crate alloc;
@@ -20,20 +22,19 @@ async fn main(_spawner: Spawner) -> ! {
     // =========== Initialization ===============
     rtt_target::rtt_init_print!();
 
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let peripherals = esp_hal::init(config);
+    let config = Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = init(config);
 
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 65536);
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let sw_interrupt =
-        esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
-    esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
+    let sw_interrupt = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+    start(timg0.timer0, sw_interrupt.software_interrupt0);
     rprintln!("Embassy initialized!");
 
     // =========== Application ===============
     loop {
         rprintln!("Hello from ESP32C6 Chip");
-        embassy_time::Timer::after_secs(1).await;
+        Timer::after_secs(1).await;
     }
 }
