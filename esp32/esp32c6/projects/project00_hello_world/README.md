df6088ade0254b55973d1a5189c9365ef0222d92
Dhruv Mohindru <dmohindru@gmail.com>
2026-06-04 18:32:33 +1000

    Almost finished writing hello world program

--- esp32/esp32c6/projects/project00_hello_world/README.md
+++ esp32/esp32c6/projects/project00_hello_world/README.md
@@ -1,3 +1,7 @@
 # Introduction
 
 This project serves as the hello world program for esp32 platform
+
+# Links
+
+https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description
--- esp32/esp32c6/projects/project00_hello_world/src/main.rs
+++ esp32/esp32c6/projects/project00_hello_world/src/main.rs
@@ -1,38 +1,23 @@
 #![no_std]
 #![no_main]
-#![deny(
-    clippy::mem_forget,
-    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
-    holding buffers for the duration of a data transfer."
-)]
-#![deny(clippy::large_stack_frames)]
 
 use embassy_executor::Spawner;
 use esp_hal::clock::CpuClock;
 use esp_hal::timer::timg::TimerGroup;
 use rtt_target::rprintln;
 
+extern crate alloc;
+
 #[panic_handler]
 fn panic(panic_info: &core::panic::PanicInfo) -> ! {
     rprintln!("{}", panic_info);
     loop {}
 }
-
-extern crate alloc;
-
-// This creates a default app-descriptor required by the esp-idf bootloader.
-// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
-#[allow(
-    clippy::large_stack_frames,
-    reason = "it's not unusual to allocate larger buffers etc. in main"
-)]
 #[esp_rtos::main]
-async fn main(spawner: Spawner) -> ! {
-    // generator version: 1.3.0
-    // generator parameters: --chip esp32c6 -o unstable-hal -o alloc -o embassy -o probe-rs
-
+async fn main(_spawner: Spawner) -> ! {
+    // =========== Initialization ===============
     rtt_target::rtt_init_print!();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
@@ -44,16 +29,11 @@ async fn main(spawner: Spawner) -> ! {
     let sw_interrupt =
         esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
     esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
-
     rprintln!("Embassy initialized!");
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
-
+    // =========== Application ===============
     loop {
         rprintln!("Hello from ESP32C6 Chip");
         embassy_time::Timer::after_secs(1).await;
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.1.0/examples
 }
