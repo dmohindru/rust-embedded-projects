ae9a8eac83e747a20031fd5982126c980ad7e7e3
Dhruv Mohindru <dmohindru@gmail.com>
2026-07-08 22:44:15 +1000

    Initial commit for the driver

--- embedded-core/src/display_driver/mod.rs
+++ embedded-core/src/display_driver/mod.rs
@@ -1,6 +1,7 @@
 mod ht16k33_driver;
 mod led_matrix_driver;
 mod max7219_driver;
+mod ssd1306;
 pub use ht16k33_driver::Ht16K33;
 pub use led_matrix_driver::LedMatrixDriver;
 pub use max7219_driver::Max7219;
