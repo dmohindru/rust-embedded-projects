1955efebb977343c41a1e660ce39c0685dfedb00
Dhruv Mohindru <dmohindru@gmail.com>
2026-08-07 21:13:56 +1000

    make a good progress

--- embedded-core/src/service/bouncing_ball/bouncing_ball.rs
+++ embedded-core/src/service/bouncing_ball/bouncing_ball.rs
@@ -20,6 +20,20 @@ impl<D, const WIDTH: usize, const HEIGHT: usize, C: PixelColor> BouncingBall<D, 
 where
     D: DrawTarget<Color = C, Error = Infallible> + Present,
 {
+    pub fn new(
+        display: D,
+        radius: usize,
+        step_size: usize,
+        ball_color: C,
+        background_color: C,
+    ) -> Self {
+        Self {
+            bouncing_ball_core: BouncingBallCore::<WIDTH, HEIGHT>::new(radius, step_size),
+            renderer: BouncingBallRenderer::<C>::new(ball_color, background_color),
+            display: display,
+        }
+    }
+
     pub async fn update(&mut self) -> Result<(), D::PresentError> {
         self.bouncing_ball_core.tick();
 
@@ -35,5 +49,82 @@ where
 
         self.display.present().await?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use embedded_graphics::{
+        geometry::{OriginDimensions, Size},
+        pixelcolor::BinaryColor,
+    };
+    use embedded_graphics_core::draw_target::DrawTarget;
+    use embedded_graphics_simulator::{
+        BinaryColorTheme, OutputSettingsBuilder, SimulatorDisplay, Window,
+    };
+
+    pub struct SimulatorDisplayPresenter {
+        display: SimulatorDisplay<BinaryColor>,
+        window: Window,
+    }
+
+    impl SimulatorDisplayPresenter {
+        pub fn new(width: u32, height: u32) -> Self {
+            let display = SimulatorDisplay::<BinaryColor>::new(Size::new(width, height));
+
+            let output_settings = OutputSettingsBuilder::new()
+                .theme(BinaryColorTheme::OledWhite)
+                .build();
+
+            let window = Window::new("Bouncing Ball", &output_settings);
+            Self { display, window }
+        }
+    }
+
+    impl DrawTarget for SimulatorDisplayPresenter {
+        type Color = BinaryColor;
+        type Error = Infallible;
+
+        fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
+        where
+            I: IntoIterator<Item = embedded_graphics::prelude::Pixel<Self::Color>>,
+        {
+            self.display.draw_iter(pixels)
+        }
+    }
+
+    impl OriginDimensions for SimulatorDisplayPresenter {
+        fn size(&self) -> Size {
+            Size::new(128, 64)
+        }
+    }
+
+    impl Present for SimulatorDisplayPresenter {
+        type PresentError = Infallible;
+
+        async fn present(&mut self) -> Result<(), Self::PresentError> {
+            self.window.update(&self.display);
+
+            Ok(())
+        }
+    }
+
+    #[tokio::test]
+    #[ignore = "visual test only"]
+    async fn visual_test_bouncing_ball_service() {
+        let simulator_presenter = SimulatorDisplayPresenter::new(128, 64);
+        let mut bouncing_ball = BouncingBall::<_, 128, 64, BinaryColor>::new(
+            simulator_presenter,
+            3,
+            5,
+            BinaryColor::On,
+            BinaryColor::Off,
+        );
+        loop {
+            bouncing_ball.update().await.unwrap();
+            tokio::time::sleep(std::time::Duration::from_millis(250)).await;
+        }
     }
 }
